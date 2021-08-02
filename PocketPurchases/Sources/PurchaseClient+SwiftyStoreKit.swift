377ab12215b14f335d2aa148d4ca62012155f7df
sroik <vasili.kazhanouski@gmail.com>
2021-08-02 20:55:56 +0300

    fix

--- PocketPurchases/Sources/PurchaseClient+SwiftyStoreKit.swift
+++ PocketPurchases/Sources/PurchaseClient+SwiftyStoreKit.swift
@@ -2,288 +2,292 @@
 //  Copyright © 2020 pocket-ninja. All rights reserved.
 //
 
-import Foundation
-import RxRelay
-import RxSwift
-import SwiftyStoreKit
+#if os(iOS)
 
-extension PurchasesClient {
-    public static func swiftyStoreKit(storeSecret: @escaping () -> String) -> PurchasesClient {
-        let service = SwiftyPurchasesService(storeSecret: storeSecret)
+    import Foundation
+    import RxRelay
+    import RxSwift
+    import SwiftyStoreKit
 
-        return PurchasesClient(
-            state: { service.state },
-            delegate: { service.delegate },
-            setup: service.setup,
-            loadProducts: service.loadProducts(_:then:),
-            restorePurhcases: service.restorePurchases(then:),
-            purchaseProduct: service.purchaseProduct(with:then:)
-        )
-    }
-}
+    extension PurchasesClient {
+        public static func swiftyStoreKit(storeSecret: @escaping () -> String) -> PurchasesClient {
+            let service = SwiftyPurchasesService(storeSecret: storeSecret)
 
-final class SwiftyPurchasesService {
-    var userDefaults = UserDefaults.standard
-    var workingQueue = DispatchQueue(label: "com.pocket-ninja.purchases.swifty.working-queue")
-    var completionQueue = DispatchQueue.main
-
-    var state: PurchasesClient.PurchasesState {
-        get {
-            userDefaults.purchasesState
-        }
-
-        set {
-            userDefaults.purchasesState = newValue
-            delegateRelay.accept(.didChangeState(newValue))
+            return PurchasesClient(
+                state: { service.state },
+                delegate: { service.delegate },
+                setup: service.setup,
+                loadProducts: service.loadProducts(_:then:),
+                restorePurhcases: service.restorePurchases(then:),
+                purchaseProduct: service.purchaseProduct(with:then:)
+            )
         }
     }
 
-    var delegate: Observable<PurchasesClient.DelegateEvent> {
-        delegateRelay.asObservable().subscribeOn(MainScheduler.instance)
-    }
+    final class SwiftyPurchasesService {
+        var userDefaults = UserDefaults.standard
+        var workingQueue = DispatchQueue(label: "com.pocket-ninja.purchases.swifty.working-queue")
+        var completionQueue = DispatchQueue.main
 
-    init(storeSecret: @escaping () -> String) {
-        self.storeSecret = storeSecret
-    }
-
-    func setup() {
-        proceedPurchases()
-        validatePurchases()
-
-        willEnterForegroundNotificationCancellable = NotificationCenter.default
-            .addObserver(forName: UIApplication.willEnterForegroundNotification,
-                         object: nil,
-                         queue: .main) { [weak self] _ in
-                self?.validatePurchases()
+        var state: PurchasesClient.PurchasesState {
+            get {
+                userDefaults.purchasesState
             }
-    }
 
-    func loadProducts(
-        _ ids: [PurchaseProduct.ID],
-        then completion: @escaping (Result<[PurchaseProduct], Error>) -> Void
-    ) {
-        workingQueue.async {
-            let cachedProducts = ids.compactMap { self.cache[$0] }
-            if cachedProducts.count == ids.count {
-                self.completionQueue.async {
-                    completion(.success(cachedProducts))
+            set {
+                userDefaults.purchasesState = newValue
+                delegateRelay.accept(.didChangeState(newValue))
+            }
+        }
+
+        var delegate: Observable<PurchasesClient.DelegateEvent> {
+            delegateRelay.asObservable().subscribeOn(MainScheduler.instance)
+        }
+
+        init(storeSecret: @escaping () -> String) {
+            self.storeSecret = storeSecret
+        }
+
+        func setup() {
+            proceedPurchases()
+            validatePurchases()
+
+            willEnterForegroundNotificationCancellable = NotificationCenter.default
+                .addObserver(forName: UIApplication.willEnterForegroundNotification,
+                             object: nil,
+                             queue: .main) { [weak self] _ in
+                    self?.validatePurchases()
                 }
-                return
+        }
+
+        func loadProducts(
+            _ ids: [PurchaseProduct.ID],
+            then completion: @escaping (Result<[PurchaseProduct], Error>) -> Void
+        ) {
+            workingQueue.async {
+                let cachedProducts = ids.compactMap { self.cache[$0] }
+                if cachedProducts.count == ids.count {
+                    self.completionQueue.async {
+                        completion(.success(cachedProducts))
+                    }
+                    return
+                }
+
+                _ = SwiftyStoreKit.retrieveProductsInfo(Set(ids)) { result in
+                    self.workingQueue.async {
+                        self.handle(result, ofRetriving: ids, with: completion)
+                    }
+                }
+            }
+        }
+
+        private func handle(
+            _ results: RetrieveResults,
+            ofRetriving ids: [PurchaseProduct.ID],
+            with completion: @escaping (Result<[PurchaseProduct], Error>) -> Void
+        ) {
+            let products: [PurchaseProduct] = ids.compactMap { id in
+                results.retrievedProducts
+                    .first { $0.productIdentifier == id }
+                    .flatMap(PurchaseProduct.init(product:))
             }
 
-            _ = SwiftyStoreKit.retrieveProductsInfo(Set(ids)) { result in
+            products.forEach { cache[$0.id] = $0 }
+
+            completionQueue.async {
+                if let error = results.error, products.isEmpty {
+                    completion(.failure(error))
+                } else {
+                    completion(.success(products))
+                }
+            }
+        }
+
+        func restorePurchases(then completion: @escaping (Result<[PurchaseProduct.ID], PurchasesClient.RestoreError>) -> Void) {
+            SwiftyStoreKit.restorePurchases { results in
                 self.workingQueue.async {
-                    self.handle(result, ofRetriving: ids, with: completion)
+                    self.handle(results, with: completion)
                 }
             }
         }
-    }
 
-    private func handle(
-        _ results: RetrieveResults,
-        ofRetriving ids: [PurchaseProduct.ID],
-        with completion: @escaping (Result<[PurchaseProduct], Error>) -> Void
-    ) {
-        let products: [PurchaseProduct] = ids.compactMap { id in
-            results.retrievedProducts
-                .first { $0.productIdentifier == id }
-                .flatMap(PurchaseProduct.init(product:))
-        }
+        private func handle(
+            _ results: RestoreResults,
+            with completion: @escaping (Result<[PurchaseProduct.ID], PurchasesClient.RestoreError>) -> Void
+        ) {
+            if results.restoredPurchases.count > 0 {
+                let restoredIds = results.restoredPurchases.map(\.productId)
+                let restoredState = restoredIds.map { ($0, PurchaseStatus.purchased) }
+                state.merge(restoredState, uniquingKeysWith: { _, rhs in rhs })
 
-        products.forEach { cache[$0.id] = $0 }
-
-        completionQueue.async {
-            if let error = results.error, products.isEmpty {
-                completion(.failure(error))
+                completionQueue.async {
+                    self.delegateRelay.accept(.didRestoreProducts(restoredIds))
+                    completion(.success(restoredIds))
+                }
             } else {
-                completion(.success(products))
-            }
-        }
-    }
+                let isFailedToRestore = results.restoreFailedPurchases.count > 0
+                let error: PurchasesClient.RestoreError = isFailedToRestore ? .failedToRestore : .nothingToRestore
 
-    func restorePurchases(then completion: @escaping (Result<[PurchaseProduct.ID], PurchasesClient.RestoreError>) -> Void) {
-        SwiftyStoreKit.restorePurchases { results in
-            self.workingQueue.async {
-                self.handle(results, with: completion)
-            }
-        }
-    }
-
-    private func handle(
-        _ results: RestoreResults,
-        with completion: @escaping (Result<[PurchaseProduct.ID], PurchasesClient.RestoreError>) -> Void
-    ) {
-        if results.restoredPurchases.count > 0 {
-            let restoredIds = results.restoredPurchases.map(\.productId)
-            let restoredState = restoredIds.map { ($0, PurchaseStatus.purchased) }
-            state.merge(restoredState, uniquingKeysWith: { _, rhs in rhs })
-
-            completionQueue.async {
-                self.delegateRelay.accept(.didRestoreProducts(restoredIds))
-                completion(.success(restoredIds))
-            }
-        } else {
-            let isFailedToRestore = results.restoreFailedPurchases.count > 0
-            let error: PurchasesClient.RestoreError = isFailedToRestore ? .failedToRestore : .nothingToRestore
-
-            completionQueue.async {
-                completion(.failure(error))
-            }
-        }
-    }
-
-    func purchaseProduct(with id: PurchaseProduct.ID, then completion: @escaping (Result<PurchaseTransaction, PurchasesClient.PurchaseError>) -> Void) {
-        SwiftyStoreKit.purchaseProduct(id) { result in
-            self.workingQueue.async {
-                self.handle(result, with: completion)
-            }
-        }
-    }
-
-    private func handle(
-        _ results: PurchaseResult,
-        with completion: @escaping (Result<PurchaseTransaction, PurchasesClient.PurchaseError>) -> Void
-    ) {
-        completionQueue.async {
-            switch results {
-            case let .success(purchase: details):
-                self.completionQueue.async {
-                    self.state[details.productId] = .purchased
+                completionQueue.async {
+                    completion(.failure(error))
                 }
-
-                let transaction = PurchaseTransaction(
-                    product: details.product,
-                    quantity: details.quantity,
-                    transactionIdentifier: details.transaction.transactionIdentifier
-                )
-                self.delegateRelay.accept(.didPurchaseProduct(transaction))
-                completion(.success(transaction))
-
-            case let .error(error):
-                let purchaseError = PurchasesClient.PurchaseError(error)
-                completion(.failure(purchaseError))
             }
         }
-    }
 
-    private func validatePurchases() {
-        let validator = AppleReceiptValidator(
-            service: .production,
-            sharedSecret: storeSecret()
-        )
-
-        SwiftyStoreKit.verifyReceipt(using: validator) { results in
-            self.handle(results)
-        }
-    }
-
-    private func handle(_ results: VerifyReceiptResult) {
-        guard
-            case let .success(receipt: receipt) = results,
-            let ids = SwiftyStoreKit.getDistinctPurchaseIds(inReceipt: receipt)
-        else {
-            return
+        func purchaseProduct(with id: PurchaseProduct.ID, then completion: @escaping (Result<PurchaseTransaction, PurchasesClient.PurchaseError>) -> Void) {
+            SwiftyStoreKit.purchaseProduct(id) { result in
+                self.workingQueue.async {
+                    self.handle(result, with: completion)
+                }
+            }
         }
 
-        loadProducts(Array(ids)) { result in
-            guard let products = try? result.get() else {
+        private func handle(
+            _ results: PurchaseResult,
+            with completion: @escaping (Result<PurchaseTransaction, PurchasesClient.PurchaseError>) -> Void
+        ) {
+            completionQueue.async {
+                switch results {
+                case let .success(purchase: details):
+                    self.completionQueue.async {
+                        self.state[details.productId] = .purchased
+                    }
+
+                    let transaction = PurchaseTransaction(
+                        product: details.product,
+                        quantity: details.quantity,
+                        transactionIdentifier: details.transaction.transactionIdentifier
+                    )
+                    self.delegateRelay.accept(.didPurchaseProduct(transaction))
+                    completion(.success(transaction))
+
+                case let .error(error):
+                    let purchaseError = PurchasesClient.PurchaseError(error)
+                    completion(.failure(purchaseError))
+                }
+            }
+        }
+
+        private func validatePurchases() {
+            let validator = AppleReceiptValidator(
+                service: .production,
+                sharedSecret: storeSecret()
+            )
+
+            SwiftyStoreKit.verifyReceipt(using: validator) { results in
+                self.handle(results)
+            }
+        }
+
+        private func handle(_ results: VerifyReceiptResult) {
+            guard
+                case let .success(receipt: receipt) = results,
+                let ids = SwiftyStoreKit.getDistinctPurchaseIds(inReceipt: receipt)
+            else {
                 return
             }
 
-            self.workingQueue.async {
-                let verifiedState = products.map { product in
-                    (product.id, SwiftyStoreKit.verifyProduct(product, inReceipt: receipt))
+            loadProducts(Array(ids)) { result in
+                guard let products = try? result.get() else {
+                    return
                 }
 
-                self.state.merge(verifiedState, uniquingKeysWith: { _, rhs in rhs })
+                self.workingQueue.async {
+                    let verifiedState = products.map { product in
+                        (product.id, SwiftyStoreKit.verifyProduct(product, inReceipt: receipt))
+                    }
+
+                    self.state.merge(verifiedState, uniquingKeysWith: { _, rhs in rhs })
+                }
+            }
+        }
+
+        private func proceedPurchases() {
+            SwiftyStoreKit.completeTransactions { purchases in
+                self.workingQueue.async {
+                    self.handle(purchases)
+                }
+            }
+        }
+
+        private func handle(_ purchases: [Purchase]) {
+            let completedState = purchases
+                .filter { purchase in
+                    let state = purchase.transaction.transactionState
+                    return state == .purchased || state == .restored
+                }
+                .map { purchase in
+                    (purchase.productId, PurchaseStatus.purchased)
+                }
+
+            state.merge(completedState, uniquingKeysWith: { _, rhs in rhs })
+            delegateRelay.accept(.didProceedProducts(completedState.map { $0.0 }))
+        }
+
+        private let storeSecret: () -> String
+        private var cache: [PurchaseProduct.ID: PurchaseProduct] = [:]
+        private var willEnterForegroundNotificationCancellable: Any?
+        private let delegateRelay = PublishRelay<PurchasesClient.DelegateEvent>()
+    }
+
+    private extension UserDefaults {
+        var purchasesState: PurchasesClient.PurchasesState {
+            get {
+                guard
+                    let data = data(forKey: .purchasesStateKey),
+                    let state = try? data.decoded() as PurchasesClient.PurchasesState
+                else {
+                    return [:]
+                }
+
+                return state
+            }
+            set {
+                if let data = try? newValue.encoded() {
+                    set(data, forKey: .purchasesStateKey)
+                }
             }
         }
     }
 
-    private func proceedPurchases() {
-        SwiftyStoreKit.completeTransactions { purchases in
-            self.workingQueue.async {
-                self.handle(purchases)
+    private extension String {
+        static let purchasesStateKey = "com.pocket-ninja.purchases.swifty.purchases-state"
+    }
+
+    private extension SwiftyStoreKit {
+        static func verifyProduct(_ product: PurchaseProduct, inReceipt receipt: ReceiptInfo) -> PurchaseStatus {
+            if product.period == .lifetime {
+                return PurchaseStatus(
+                    SwiftyStoreKit.verifyPurchase(productId: product.id, inReceipt: receipt)
+                )
+            } else {
+                return PurchaseStatus(
+                    SwiftyStoreKit.verifySubscription(ofType: .autoRenewable, productId: product.id, inReceipt: receipt)
+                )
             }
         }
     }
 
-    private func handle(_ purchases: [Purchase]) {
-        let completedState = purchases
-            .filter { purchase in
-                let state = purchase.transaction.transactionState
-                return state == .purchased || state == .restored
+    private extension PurchaseStatus {
+        init(_ result: VerifyPurchaseResult) {
+            switch result {
+            case .notPurchased:
+                self = .notPurchased
+            case .purchased:
+                self = .purchased
             }
-            .map { purchase in
-                (purchase.productId, PurchaseStatus.purchased)
-            }
-
-        state.merge(completedState, uniquingKeysWith: { _, rhs in rhs })
-        delegateRelay.accept(.didProceedProducts(completedState.map { $0.0 }))
-    }
-
-    private let storeSecret: () -> String
-    private var cache: [PurchaseProduct.ID: PurchaseProduct] = [:]
-    private var willEnterForegroundNotificationCancellable: Any?
-    private let delegateRelay = PublishRelay<PurchasesClient.DelegateEvent>()
-}
-
-private extension UserDefaults {
-    var purchasesState: PurchasesClient.PurchasesState {
-        get {
-            guard
-                let data = data(forKey: .purchasesStateKey),
-                let state = try? data.decoded() as PurchasesClient.PurchasesState
-            else {
-                return [:]
-            }
-
-            return state
         }
-        set {
-            if let data = try? newValue.encoded() {
-                set(data, forKey: .purchasesStateKey)
+
+        init(_ result: VerifySubscriptionResult) {
+            switch result {
+            case .notPurchased:
+                self = .notPurchased
+            case .expired:
+                self = .expired
+            case .purchased:
+                self = .purchased
             }
         }
     }
-}
 
-private extension String {
-    static let purchasesStateKey = "com.pocket-ninja.purchases.swifty.purchases-state"
-}
-
-private extension SwiftyStoreKit {
-    static func verifyProduct(_ product: PurchaseProduct, inReceipt receipt: ReceiptInfo) -> PurchaseStatus {
-        if product.period == .lifetime {
-            return PurchaseStatus(
-                SwiftyStoreKit.verifyPurchase(productId: product.id, inReceipt: receipt)
-            )
-        } else {
-            return PurchaseStatus(
-                SwiftyStoreKit.verifySubscription(ofType: .autoRenewable, productId: product.id, inReceipt: receipt)
-            )
-        }
-    }
-}
-
-private extension PurchaseStatus {
-    init(_ result: VerifyPurchaseResult) {
-        switch result {
-        case .notPurchased:
-            self = .notPurchased
-        case .purchased:
-            self = .purchased
-        }
-    }
-
-    init(_ result: VerifySubscriptionResult) {
-        switch result {
-        case .notPurchased:
-            self = .notPurchased
-        case .expired:
-            self = .expired
-        case .purchased:
-            self = .purchased
-        }
-    }
-}
+#endif
